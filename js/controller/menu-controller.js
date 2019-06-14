ee0432c4eb133338691d42681330388de8e32efc
Tiago Luz <tiagoluz.h607@gmail.com>
2019-06-14 14:08:56 -0300

    incluindo datatables

---
+++ js/controller/menu-controller.js
@@ -0,0 +1,15 @@
+function goToCadastroAluno(){
+    window.location.href = './views/aluno/alunos.html';
+}
+
+function goToCadastroProfessor(){
+    window.location.href = './views/professor/professores.html';
+}
+
+function goToLancamentoNotas(){
+    window.location.href = './views/avaliacao/avaliacoes.html';
+}
+
+function goToRelatorioAprovados(){
+    window.location.href = './views/relatorio/aprovados.html';
+}
