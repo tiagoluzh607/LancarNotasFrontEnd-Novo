bf0d627c20fa3a28cfd6dc7477f4e47a4653098c
Tiago Luz <tiagoluz.h607@gmail.com>
2019-06-14 15:47:52 -0300

    Code - incluindo um aluno estaticamente

--- js/controller/formaluno-controller.js
+++ js/controller/formaluno-controller.js
@@ -5,6 +5,25 @@ function goToAlunos(){
 
 function adicionaAlunoApi(){
 
+    $.ajaxSetup({
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        }
+    });
+
+
+    let aluno = {nome: "Tiago Luz", rg: "12354263"};
+    let alunoJsonString = JSON.stringify(aluno);
+
+    $.post("http://184.107.24.224:12301/api/alunos", alunoJsonString, function(aluno){
+        
+        alert('Aluno com id: '+aluno.id+' salvo com sucesso');
+        window.location.replace('alunos.html');
     
+    }).fail(function(error){
+        console.error(error);
+        alert('Não foi possivel salvar aluno na API');
+    })
 
 }
