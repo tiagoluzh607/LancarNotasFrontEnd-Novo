c4e6d353ef8d14d59593875edc5c7cbf924a7c83
Tiago Luz <tiagoluz.h607@gmail.com>
2019-06-21 18:30:45 -0300

    Metodo para deletar alunos da lista

--- js/controller/professores-controller.js
+++ js/controller/professores-controller.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
         for(i in professores){
             let professor = professores[i]
 
-            let linha = [professor.id, professor.nome, professor.rg, professor.escolaridade];
+            let linha = [professor.id, professor.nome, professor.rg, professor.escolaridade , `{ "id": `+professor.id+`}`];
     
             dados.push(linha);
         }
@@ -23,7 +23,29 @@ $(document).ready(function() {
                 { title: "Id" },
                 { title: "Nome" },
                 { title: "Rg" },
-                { title: "Escolaridade" }
+                { title: "Escolaridade" },
+                { title: "Ações",
+                        className: "td-actions text-right",
+                        render: function ( data, type, row, meta ) {
+                            
+                        let dataJson;
+                        try{
+                            //console.log(JSON.parse(data));
+                            dataJson = JSON.parse(data)
+                        }catch(err){
+                            console.error(err);
+                        } 
+                            
+                            return `
+                            
+                            <button onclick="deletaProfessor(`+dataJson.id+`)" rel="tooltip" class="login100-form-btn btn-danger"  title="Excluir">
+                                <i class="fa fa-trash" aria-hidden="true"></i>
+                            </button>
+
+                            `;
+                            
+                        }
+                }
             ]
         } );
 
@@ -36,5 +58,36 @@ $(document).ready(function() {
 
 function goToFormProfessor(){
     window.location.replace('formprofessor.html');
+}
+
+jQuery.each( [ "put", "delete" ], function( i, method ) {
+    jQuery[ method ] = function( url, data, callback, type ) {
+      if ( jQuery.isFunction( data ) ) {
+        type = type || callback;
+        callback = data;
+        data = undefined;
+      }
+  
+      return jQuery.ajax({
+        url: url,
+        type: method,
+        dataType: type,
+        data: data,
+        success: callback
+      });
+    };
+});
+
+function deletaProfessor(id){
+    
+    $.delete("http://184.107.24.224:12301/api/professores/"+id, null,function(professor){
+        
+        alert('Professor com id: '+professor.id+' Foi Excluído');
+        window.location.replace('professores.html');
+    
+    }).fail(function(error){
+        console.error(error);
+        alert('Não foi deletar o professor na API');
+    })
 }
 
